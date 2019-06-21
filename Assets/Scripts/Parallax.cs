2604fe45cfeb2c6c9019a479b6d630d9976d2170
Isaac Augusto <isaac.augusto@hotmail.com>
2019-06-21 12:31:07 -0300

    ADDED BoxLevel and Parallax

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private GameObject _cam;
+    [SerializeField] private float _parallax;
+    private float _lenght, _startPos;
+
+    void Start()
+    {
+        _startPos = transform.position.x;
+        _lenght = GetComponentInChildren<SpriteRenderer>().bounds.size.x;
+    }
+
+    void FixedUpdate()
+    {
+        float dist = (_cam.transform.position.x * _parallax);
+
+        Vector3 pos = new Vector3(_startPos + dist, transform.position.y, transform.position.z);
+
+        transform.position = Vector3.Lerp(transform.position, pos, 1);
+    }
+}
